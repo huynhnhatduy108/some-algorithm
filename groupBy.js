c3d533b46bd72b7bdc76e9d2ba1d855db0ffe61d
huynhnhatduy108 <36949451+huynhnhatduy108@users.noreply.github.com>
2022-05-10 15:57:57 +0700

    Update groupBy.js

--- groupBy.js
+++ groupBy.js
@@ -1,1 +1,3 @@
+const cars = ["Saab", "Volvo", "BMW","Mec","Ferrari","Volvo", "Mec","BMW","Toyota", "Huyndai", "Mec" ];
+const occ = cars.reduce((acc, current)=>{return {...acc,[current]:(acc[current]||0)+1}},{})
 
